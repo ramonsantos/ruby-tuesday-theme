ad3b06d633ffbc9133596a8128ed252f3cd34aa6
ramonsantos <ramonsantos.pe@gmail.com>
2016-12-29 16:21:44 -0300

    Organizada nos arquivos JS.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,17 @@ module.exports = function( grunt ) {
             'source/metronic/assets/corporate/css/themes/red.css',
             'source/css/theme.css' ],
         dest: 'source/css/css-full.css'
+      },
+
+      js: {
+        options: {
+          separator: ';',
+        },
+        src: [
+            'source/js/_layout.js',
+            'source/js/_back-to-top.js'
+             ],
+        dest: 'source/js/js-full.min.js'
       }
     },
 
@@ -26,6 +37,6 @@ module.exports = function( grunt ) {
   grunt.loadNpmTasks('grunt-contrib-stylus');
   grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.registerTask( 'default', [  'stylus', 'concat:css' ] );
-  
+  grunt.registerTask( 'default', [  'stylus', 'concat:css' , 'concat:js'] );
+
 };
